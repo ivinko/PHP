158fbe08e11f35206d3fac0b13a637c820b637df
ivinko <38477708+ivinko@users.noreply.github.com>
2018-06-22 20:19:26 +0200

    function

--- spirala.php
+++ spirala.php
@@ -5,57 +5,58 @@ $redova=15;
 $kolona=10;
 
 
-
-$podaci = array();
-for($i=0;$i<$redova;$i++)
-{
+function napuniMatricu($redova,$kolona){
+    $podaci = array();
+    for($i=0;$i<$redova;$i++)
+    {
     $kolone=array();
     for($j=0;$j<$kolona;$j++){
     $kolone[]=0;
     }
     $podaci[]=$kolone;
+    }
+
+    //napuni
+    $brojac=1;
+    $a=1;
+
+    while($a<=$redova*$kolona){
+
+        for($i=$kolona-$a; $i>=($a-1);$i--)
+        {
+        if($brojac>$redova*$kolona){
+        return $podaci;
+        }
+        $podaci[$redova-$a][$i]=$brojac++;
+        }
+
+        for($i=$redova-($a+1); $i>=($a-1);$i--)
+        {
+        if($brojac>$redova*$kolona){
+        return $podaci;
+        }
+        $podaci[$i][$a-1]=$brojac++;
+        }
+        for($i=$a;$i<=$kolona-$a;$i++)
+        {
+        if($brojac>$redova*$kolona){
+        return $podaci;
+        }
+        $podaci[$a-1][$i]=$brojac++;
+        }
+        for($i=$a; $i<=$redova-($a+1);$i++)
+        {
+        if($brojac>$redova*$kolona){
+        return $podaci;
+        }
+        $podaci[$i][$kolona-$a]=$brojac++;
+        }
+        $a++;
+    }
+    return $podaci;
 }
 
-//napuni
-$brojac=1;
-$a=1;
-
-while($a<=$redova*$kolona){
-
-for($i=$kolona-$a; $i>=($a-1);$i--)
-{
-    if($brojac>$redova*$kolona){
-        goto nastavi;
-     }
-    $podaci[$redova-$a][$i]=$brojac++;
-}
-
-for($i=$redova-($a+1); $i>=($a-1);$i--)
-{
-    if($brojac>$redova*$kolona){
-        goto nastavi;
-     }
-    $podaci[$i][$a-1]=$brojac++;
-}
-for($i=$a;$i<=$kolona-$a;$i++)
-{
-    if($brojac>$redova*$kolona){
-        goto nastavi;
-     }
-    $podaci[$a-1][$i]=$brojac++;
-}
-for($i=$a; $i<=$redova-($a+1);$i++)
-{
-    if($brojac>$redova*$kolona){
-        goto nastavi;
-     }
-    $podaci[$i][$kolona-$a]=$brojac++;
-}
-$a++;
-}
-
-
-nastavi:
+$podaci = napuniMatricu($redova,$kolona);
 
 echo "<table>";
 for($i=0;$i<$redova;$i++)
